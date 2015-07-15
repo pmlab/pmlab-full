76c5ecb543c365b08aabbcb1df955ce51beff4c2
Javier <dev.git@javispedro.com>
2015-07-15 12:39:22 +0200

    allow non-verbose cases_included_in_ts() function, do not default to partial_cases

--- pmlab/log_ts/__init__.py
+++ pmlab/log_ts/__init__.py
@@ -1,12 +1,14 @@
 from .. log import Log
 from .. ts import TransitionSystem, IndeterminedTsError
 
-def cases_included_in_ts(log, ts, uniq_cases=False):
-    """Returns a log containing only the cases (and parts of cases) that fit 
-    in the TS. Prints information on how the cases of a log can be reproduced by
+def cases_included_in_ts(log, ts, uniq_cases=False, partial_cases=False, verbose=False):
+    """Returns a log containing only the cases (and optionally parts of cases) that fit
+    in the TS.
+    [uniq_cases] If True, consider only unique cases.
+    [partial_cases] If True, consider also cases that are only partially covered.
+    [verbose] If True, prints information on how the cases of a log can be reproduced by
     a TS or not.
-    
-    [uniq_cases] If True, consider only unique cases."""
+    """
     partially_included=0
     totally_included=0
     excluded_traces=0
@@ -36,22 +38,24 @@ def cases_included_in_ts(log, ts, uniq_cases=False):
         else:
             ratio += i/((1.0)*len(case))*occ
             partially_included += occ
-        if i != 0:
+        if i == len(case) or (partial_cases and i > 0):
             saved_cases[tuple(case[:i])] = occ
-    num_cases = len(cases)
-    width=len(str(num_cases))
-    print "Total cases in log:  {0:{width}}".format(num_cases, width=width)
-    print "Excluded:            {0:{width}} {1:7.2%}".format(excluded_traces, 
-                                                            excluded_traces / (1.0*num_cases),
-                                                            width=width )
-    print "Partially Included:  {0:{width}} {1:7.2%}".format(partially_included, 
-                                                partially_included / (1.0*num_cases),
-                                                width=width),
-    if partially_included:
-        print "average included length {0:5.2%}".format(ratio/(1.0*partially_included))
-    else:
-        print
-    print "Totally Included:    {0:{width}} {1:7.2%}".format(totally_included, 
-                                                            totally_included / (1.0*num_cases),
-                                                            width=width )
+
+    if verbose:
+        num_cases = len(cases)
+        width=len(str(num_cases))
+        print "Total cases in log:  {0:{width}}".format(num_cases, width=width)
+        print "Excluded:            {0:{width}} {1:7.2%}".format(excluded_traces,
+                                                                excluded_traces / (1.0*num_cases),
+                                                                width=width )
+        print "Partially Included:  {0:{width}} {1:7.2%}".format(partially_included,
+                                                    partially_included / (1.0*num_cases),
+                                                    width=width),
+        if partially_included:
+            print "average included length {0:5.2%}".format(ratio/(1.0*partially_included))
+        else:
+            print
+        print "Totally Included:    {0:{width}} {1:7.2%}".format(totally_included,
+                                                                totally_included / (1.0*num_cases),
+                                                                width=width )
     return Log(uniq_cases=saved_cases)
